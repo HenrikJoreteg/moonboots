13c7e74b2d2e12fe1c59e94ea454cb7127bbbf32
Luke Karrys <lukekarrys@gmail.com>
2013-09-10 00:09:07 -0700

    make sample app work and a bit more concise (remove unused files)

--- sample/app/app.js
+++ sample/app/app.js
@@ -1,11 +1,13 @@
 var Backbone = require('backbone');
+var templates = require('./templates');
 
 module.exports = {
     launch: function () {
         window.app = this;
         console.log("I RAN!");
         console.log("REQUIAB", Backbone);
+        console.log(templates.main('hello'));
     }
-}
+};
 
 module.exports.launch();
