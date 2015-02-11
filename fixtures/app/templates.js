d03767c5b3141c7b4c4ac044cbb2ade25ce5b9a4
Luke Karrys <lukekarrys@gmail.com>
2015-02-11 14:52:02 -0700

    add failing test for watchify when getting js bundle multiple times

---
+++ fixtures/app/templates.js
@@ -0,0 +1,4 @@
+var foo = require('foo');
+
+console.log(foo);
+if (foo !== 'foo') throw new Error('Foo is not foo.');
---
+++ test/watchify.js
@@ -0,0 +1,34 @@
+var Lab = require('lab');
+var async = require('async');
+var Moonboots = require('..');
+var path = require('path');
+
+
+Lab.test('watchify', function (done) {
+    var moonboots = new Moonboots({
+        main: path.join(__dirname, '/../fixtures', '/app/templates.js'),
+        modulesDir: path.join(__dirname, '/../fixtures', '/modules2'),
+        developmentMode: true,
+        watchify: true
+    });
+
+    var jsSource = function (cb) {
+        moonboots.jsSource(function (err, js) {
+            if (err) return cb(err, null);
+            Lab.expect(function evalSource() {
+                eval(js);
+            }).to.not.throw();
+            cb(null, js);
+        });
+    };
+
+    moonboots.on('ready', function () {
+        async.series([jsSource, jsSource], function (err, res) {
+            Lab.expect(err).to.equal(void 0);
+            Lab.expect(res[0]).to.equal(res[1]);
+            done();
+        });
+    });
+});
+
+
